07f3d2df5c5184882c4944dcaf9d288a09ca57f9
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2025-02-02 06:56:44 -0700

    feat: Add basic BirdsEyeMode.

---
+++ Unity Project/Dungeoneering/Assets/_Libs/DungeonCrawler.Unity/src/CameraModes/BirdsEyeMode.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using CaptainCoder.Dungeoneering.Player;
+using UnityEngine;
+
+namespace CaptainCoder.Dungeoneering.Unity
+{
+    [CreateAssetMenu(menuName = "CameraModes/BirdsEyeMode")]
+    public class BirdsEyeMode : CameraMode
+    {
+        [field: SerializeField]
+        public float Duration { get; private set; } = 0.1f;
+        public Vector3 Offset { get; private set; } = new Vector3(0, 10, 0.5f);
+        public override IEnumerator HandlePlayerViewChanged(Transform transform, PlayerView exit, PlayerView enter)
+        {
+            Quaternion startQ = transform.rotation;
+            Quaternion endQ = Quaternion.Euler(90, -90, 0);
+            Vector3 start = transform.position;
+            Vector3 end = enter.Position.ToVector3() + Offset;
+            float elapsedTime = 0;
+            while (Percent() < 1)
+            {
+                transform.position = Vector3.Lerp(start, end, Percent());
+                transform.rotation = Quaternion.Lerp(startQ, endQ, Percent());
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+            transform.position = Vector3.Lerp(start, end, 1);
+            transform.rotation = Quaternion.Lerp(startQ, endQ, 1);
+            float Percent() => elapsedTime / Duration;
+        }
+
+    }
+}
