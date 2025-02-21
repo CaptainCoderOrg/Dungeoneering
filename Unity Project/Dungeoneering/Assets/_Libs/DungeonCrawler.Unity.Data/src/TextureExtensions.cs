fcb167322ba28ca5faef8cb0d1d5fda35cd839d0
Steve <125539+steve7411@users.noreply.github.com>
2025-02-21 09:54:44 -0500

    Changing graphics settings to force the selectable shader to be included in build. Throwing better error when the shader isn't found at runtime.

--- Unity Project/Dungeoneering/Assets/_Libs/DungeonCrawler.Unity.Data/src/TextureExtensions.cs
+++ Unity Project/Dungeoneering/Assets/_Libs/DungeonCrawler.Unity.Data/src/TextureExtensions.cs
@@ -7,7 +7,13 @@ namespace CaptainCoder.Dungeoneering.DungeonMap.Unity
         {
             Texture2D t2d = new(2, 2) { filterMode = FilterMode.Point };
             t2d.LoadImage(texture.Data);
-            return new Material(Shader.Find("Custom/SelectableShader"))
+            Shader shader = Shader.Find("Custom/SelectableShader");
+            if (shader == null)
+            {
+                Debug.LogErrorFormat("Can't find shader 'Custom/SelectableShader'.");
+                throw new System.Exception("Can't find shader!");
+            }
+            return new Material(shader)
             {
                 mainTexture = t2d
             };
