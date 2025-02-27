f8bc8623f5587840dd53194bc9fa31f85878249a
Captain Coder <captaincodersacademy@gmail.com>
2025-02-26 17:17:42 -0700

    chore: Explicitly use C# 10.0 as it is the latest version.

--- Unity Project/Dungeoneering/Assets/_Libs/Postprocessor/LangVersionPostprocessor.cs
+++ Unity Project/Dungeoneering/Assets/_Libs/Postprocessor/LangVersionPostprocessor.cs
@@ -11,7 +11,7 @@ internal sealed class LangVersionPostprocessor : AssetPostprocessor
     private static string OnGeneratedCSProject(string path, string content)
     {
         var pattern = @"<LangVersion>(.*?)<\/LangVersion>";
-        var replacement = "<LangVersion>latest</LangVersion>";
+        var replacement = "<LangVersion>10.0</LangVersion>";
         content = Regex.Replace(content, pattern, replacement);
         return content;
     }
