e4ab7c89a62f3298bb18d99bf27519e011728772
Captain Coder <captaincodersacademy@gmail.com>
2025-03-06 06:33:25 -0700

    refactor: Update PlayerViewController to use DungeonCrawlerData.

--- Unity Project/Dungeoneering/Assets/_Libs/DungeonCrawler.Unity/src/PlayerViewController.cs
+++ Unity Project/Dungeoneering/Assets/_Libs/DungeonCrawler.Unity/src/PlayerViewController.cs
@@ -1,6 +1,7 @@
 using CaptainCoder.Dungeoneering.DungeonMap;
 using CaptainCoder.Dungeoneering.Player;
 using CaptainCoder.Dungeoneering.Unity.Data;
+using CaptainCoder.Unity;
 
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -12,8 +13,12 @@ namespace CaptainCoder.Dungeoneering.Unity
     {
         [field: SerializeField]
         public PlayerViewData PlayerView { get; private set; }
-        [field: SerializeField]
-        public DungeonData Dungeon { get; private set; }
+        [SerializeField]
+        private DungeonCrawlerData _dungeonCrawlerData;
+        void Awake()
+        {
+            Assertion.NotNull(this, (_dungeonCrawlerData, "Dungeon Crawler Data"));
+        }
         public void HandleInput(MovementAction action)
         {
             if (Keyboard.current.shiftKey.isPressed)
@@ -21,7 +26,7 @@ namespace CaptainCoder.Dungeoneering.Unity
                 HandleInputIgnoringWalls(action);
                 return;
             }
-            PlayerView.View = PlayerControls.Move(Dungeon.Dungeon, PlayerView.View, action);
+            PlayerView.View = PlayerControls.Move(_dungeonCrawlerData.DungeonData.Dungeon, PlayerView.View, action);
         }
 
         private void HandleInputIgnoringWalls(MovementAction action)
@@ -33,7 +38,7 @@ namespace CaptainCoder.Dungeoneering.Unity
                 MovementAction.StepBackward => new PlayerView(p.Step(f.Opposite()), f),
                 MovementAction.StrafeLeft => new PlayerView(p.Step(f.RotateCounterClockwise()), f),
                 MovementAction.StrafeRight => new PlayerView(p.Step(f.Rotate()), f),
-                _ => PlayerControls.Move(Dungeon.Dungeon, PlayerView.View, action),
+                _ => PlayerControls.Move(_dungeonCrawlerData.DungeonData.Dungeon, PlayerView.View, action),
             };
             PlayerView.View = newView;
         }
