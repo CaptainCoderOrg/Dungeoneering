f8c7abc3a765b545158fb33b9042fabfa3fb1303
Captain Coder <captaincodersacademy@gmail.com>
2025-03-06 06:22:00 -0700

    refactor: Update DungeonSelectorPanel.

--- Unity Project/Dungeoneering/Assets/_Libs/DungeonCrawler.Unity.Editor/src/DungeonSelectorPanel/DungeonSelectorPanel.cs
+++ Unity Project/Dungeoneering/Assets/_Libs/DungeonCrawler.Unity.Editor/src/DungeonSelectorPanel/DungeonSelectorPanel.cs
@@ -1,6 +1,5 @@
 using CaptainCoder.Dungeoneering.DungeonCrawler;
 using CaptainCoder.Dungeoneering.DungeonMap;
-using CaptainCoder.Dungeoneering.DungeonMap.Unity;
 using CaptainCoder.Dungeoneering.Unity.Data;
 using CaptainCoder.Unity;
 using CaptainCoder.Unity.UI;
@@ -11,9 +10,7 @@ namespace CaptainCoder.Dungeoneering.Unity.Editor
     public class DungeonSelectorPanel : MonoBehaviour
     {
         [SerializeField]
-        private DungeonData _dungeonData;
-        [SerializeField]
-        private DungeonController _dungeonController;
+        private DungeonCrawlerData _dungeonCrawlerData;
         [SerializeField]
         private Transform _buttonTransform;
         [SerializeField]
@@ -24,17 +21,17 @@ namespace CaptainCoder.Dungeoneering.Unity.Editor
         private ConfirmPromptPanel _confirmPromptPanel;
         void Awake()
         {
-            Assertion.NotNull(this, _buttonTransform, _dungeonButtonPrefab, _dungeonController, _dungeonData);
+            Assertion.NotNull(this, _buttonTransform, _dungeonButtonPrefab, _dungeonCrawlerData);
         }
 
         void OnEnable()
         {
-            _dungeonController.ManifestData.AddListener(HandleManifestLoaded);
+            _dungeonCrawlerData.ManifestData.AddListener(HandleManifestLoaded);
         }
 
         void OnDisable()
         {
-            _dungeonController.ManifestData.RemoveListener(HandleManifestLoaded);
+            _dungeonCrawlerData.ManifestData.RemoveListener(HandleManifestLoaded);
         }
 
         private void HandleManifestLoaded(DungeonCrawlerManifest manifest)
@@ -43,7 +40,7 @@ namespace CaptainCoder.Dungeoneering.Unity.Editor
             foreach ((string name, Dungeon d) in manifest.Dungeons)
             {
                 DungeonSelectorButton button = Instantiate(_dungeonButtonPrefab, _buttonTransform);
-                button.Initialize(d, _dungeonController.DungeonData.Dungeon.Name == d.Name);
+                button.Initialize(d, _dungeonCrawlerData.DungeonData.Dungeon.Name == d.Name);
                 button.OnSelected.AddListener(TryOpenDungeon);
                 button.OnRemoved.AddListener(PromptDeleteDungeon);
             }
@@ -51,7 +48,7 @@ namespace CaptainCoder.Dungeoneering.Unity.Editor
 
         public void PromptDeleteDungeon(Dungeon dungeon)
         {
-            if (_dungeonController.DungeonData.Dungeon == dungeon)
+            if (_dungeonCrawlerData.DungeonData.Dungeon == dungeon)
             {
                 _confirmPromptPanel.Prompt($"Cannot delete <b>{dungeon.Name}</b> because it is currently open.", null, null);
                 return;
@@ -59,15 +56,15 @@ namespace CaptainCoder.Dungeoneering.Unity.Editor
             _confirmPromptPanel.Prompt($"Are you sure you want to delete <b>{dungeon.Name}</b>?\n<b><color=red>This cannot be undone!</color></b>", DeleteDungeon);
             void DeleteDungeon()
             {
-                _dungeonController.ManifestData.RemoveDungeon(dungeon);
+                _dungeonCrawlerData.ManifestData.RemoveDungeon(dungeon);
             }
         }
 
         private void WarnIfChanges(System.Action action)
         {
-            if (_dungeonData.HasChanged)
+            if (_dungeonCrawlerData.DungeonData.HasChanged)
             {
-                _confirmPromptPanel.Prompt($"You have unsaved changes in <b>{_dungeonData.Dungeon.Name}</b>.\nAre you sure you want to continue?\n<b><color=red>This cannot be undone!</color></b>", action);
+                _confirmPromptPanel.Prompt($"You have unsaved changes in <b>{_dungeonCrawlerData.DungeonData.Dungeon.Name}</b>.\nAre you sure you want to continue?\n<b><color=red>This cannot be undone!</color></b>", action);
             }
             else
             {
@@ -77,17 +74,16 @@ namespace CaptainCoder.Dungeoneering.Unity.Editor
 
         public void PromptCreateDungeon() => WarnIfChanges(ShowNewDungeonPrompt);
         private void TryOpenDungeon(Dungeon d) => WarnIfChanges(() => OpenDungeon(d));
-
         private void ShowNewDungeonPrompt() => _textEntryDialogPanel.Prompt("Enter dungeon name", "Create Dungeon", "Cancel", CreateNewDungeon, ValidateDungeonName);
 
         private string ValidateDungeonName(string name)
         {
-            if (!_dungeonController.ManifestData.Manifest.Dungeons.ContainsKey(name)) { return null; }
+            if (!_dungeonCrawlerData.ManifestData.Manifest.Dungeons.ContainsKey(name)) { return null; }
             return $"A dungeon named {name} already exists";
         }
         public void CreateNewDungeon(string name)
         {
-            if (_dungeonController.ManifestData.Manifest.Dungeons.ContainsKey(name))
+            if (_dungeonCrawlerData.ManifestData.Manifest.Dungeons.ContainsKey(name))
             {
                 _textEntryDialogPanel.ShowError($"A dungeon named {name} already exists.");
                 return;
@@ -95,15 +91,15 @@ namespace CaptainCoder.Dungeoneering.Unity.Editor
 
             Dungeon newDungeon = new() { Name = name };
             newDungeon.SetBorderWalls(DungeonGlobals.Positions, WallType.Solid, DungeonGlobals.AllFacings);
-            _dungeonController.ManifestData.Manifest.AddDungeon(newDungeon.Name, newDungeon);
-            _dungeonController.DungeonData.Dungeon = newDungeon.Copy();
+            _dungeonCrawlerData.ManifestData.Manifest.AddDungeon(newDungeon.Name, newDungeon);
+            _dungeonCrawlerData.DungeonData.Dungeon = newDungeon.Copy();
             Hide();
         }
 
         private void OpenDungeon(Dungeon d)
         {
             Hide();
-            _dungeonData.Dungeon = d.Copy();
+            _dungeonCrawlerData.DungeonData.Dungeon = d.Copy();
         }
 
         public void Hide() => gameObject.SetActive(false);
