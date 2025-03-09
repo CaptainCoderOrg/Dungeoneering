542aacaf72f215cd0bc1da72d6caaf08317cf404
Captain Coder <captaincodersacademy@gmail.com>
2025-03-08 18:12:36 -0700

    chore: Add SuperUnityBuild along with Windows/WebGL Conficutation

---
+++ Unity Project/Dungeoneering/Assets/SuperUnityBuild/BuildConstants.cs
@@ -0,0 +1,47 @@
+using System;
+
+// This file is auto-generated. Do not modify or move this file.
+
+namespace SuperUnityBuild.Generated
+{
+    public enum ReleaseType
+    {
+        None,
+        PrePrePrePreAlpha_Build,
+    }
+
+    public enum Platform
+    {
+        None,
+        UWP,
+    }
+
+    public enum ScriptingBackend
+    {
+        None,
+        IL2CPP,
+    }
+
+    public enum Architecture
+    {
+        None,
+        UWP,
+    }
+
+    public enum Distribution
+    {
+        None,
+    }
+
+    public static class BuildConstants
+    {
+        public static readonly DateTime buildDate = new DateTime(638770542453118262);
+        public const string version = "1.0.0.1";
+        public const ReleaseType releaseType = ReleaseType.PrePrePrePreAlpha_Build;
+        public const Platform platform = Platform.UWP;
+        public const ScriptingBackend scriptingBackend = ScriptingBackend.IL2CPP;
+        public const Architecture architecture = Architecture.UWP;
+        public const Distribution distribution = Distribution.None;
+    }
+}
+
