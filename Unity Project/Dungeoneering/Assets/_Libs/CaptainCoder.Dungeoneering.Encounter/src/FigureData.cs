6af8fde459ec0f9a8ed99baaef1378dfd945ceba
Captain Coder <captaincodersacademy@gmail.com>
2025-03-26 12:53:36 -0700

    feat: First draft of Figure Panel

--- Unity Project/Dungeoneering/Assets/_Libs/CaptainCoder.Dungeoneering.Encounter/src/FigureData.cs
+++ Unity Project/Dungeoneering/Assets/_Libs/CaptainCoder.Dungeoneering.Encounter/src/FigureData.cs
@@ -5,6 +5,8 @@ namespace CaptainCoder.Dungeoneering.Encounter
     public class FigureData : ObservableSO
     {
         [field: SerializeField] public string Name { get; private set; }
-        [field: SerializeField] public AnimationData[] Animations { get; private set; }
+        [field: SerializeField] public AnimationData SpawnAnimation { get; private set; }
+        [field: SerializeField] public AnimationData AttackAnimation { get; private set; }
+        [field: SerializeField] public AnimationData IdleAnimation { get; private set; }
     }
 }
