a5e28f2f00d70d6c66d58b3ec910f70a9c8e0242
Captain Coder <captaincodersacademy@gmail.com>
2025-03-29 17:39:53 -0700

    feat: Add stamina icon.

--- Unity Project/Dungeoneering/Assets/_Libs/CaptainCoder.Dungeoneering.Encounter/src/TooltipController.cs
+++ Unity Project/Dungeoneering/Assets/_Libs/CaptainCoder.Dungeoneering.Encounter/src/TooltipController.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+
 using CaptainCoder.Unity.Assertions;
 
 using TMPro;
@@ -20,11 +22,21 @@ namespace CaptainCoder.Dungeoneering.Encounter
         public void ShowAbove(RectTransform parent)
         {
             transform.position = parent.position;
-            Show();
+            StartCoroutine(Show());
         }
 
-        public void Show()
+        private readonly Vector3[] _corners = { default, default, default, default };
+        public IEnumerator Show()
         {
+            yield return null; 
+            // We must wait one frame for any visual changes to take place then
+            // we calculate if we are on the screen and adjust accordingly
+            RectTransform rect = (RectTransform)transform;
+            rect.GetWorldCorners(_corners);
+            if (_corners[0].x < 10)
+            {
+                rect.position += new Vector3(-_corners[0].x + 10, 0, 0);
+            }
             _canvasGroup.alpha = 1;
         }
 
