7bbdab5407a5c1f8d132860b830483ef7281f84a
Captain Coder <captaincodersacademy@gmail.com>
2025-04-09 07:51:00 -0700

    feat: Add each side to EnsureOnScreen

--- Unity Project/Dungeoneering/Assets/_Libs/CaptainCoder.Dungeoneering.Encounter/src/RectTransformExtensions.cs
+++ Unity Project/Dungeoneering/Assets/_Libs/CaptainCoder.Dungeoneering.Encounter/src/RectTransformExtensions.cs
@@ -4,13 +4,42 @@ using UnityEngine;
 
 public static class RectTransformExtensions
 {
-    private static readonly Vector3[] Corners = { default, default, default, default };
+    private static readonly Vector3[] CornerVectors = { default, default, default, default };
+    const float Padding = 10;
     public static void EnsureOnScreen(this RectTransform rect)
     {
-        rect.GetWorldCorners(Corners);
-        if (Corners[0].x < 10)
+        Corners corners = rect.FindCorners();
+        if (corners.MinX < Padding)
         {
-            rect.position += new Vector3(-Corners[0].x + 10, 0, 0);
+            rect.position += new Vector3(-corners.MinX + Padding, 0, 0);
+        }
+        if (corners.MinY < Padding)
+        {
+            rect.position += new Vector3(0, -corners.MinY + Padding, 0);
+        }
+        if (corners.MaxX > Screen.width - Padding)
+        {
+
+            rect.position -= new Vector3(corners.MaxX - Screen.width + Padding, 0, 0);
+        }
+        if (corners.MaxY > Screen.height - Padding)
+        {
+            rect.position -= new Vector3(0, corners.MaxY - Screen.height + Padding, 0);
         }
     }
+
+    private static Corners FindCorners(this RectTransform rect)
+    {
+        rect.GetWorldCorners(CornerVectors);
+        return new Corners
+        {
+            MinX = Mathf.Min(CornerVectors[0].x, CornerVectors[1].x, CornerVectors[2].x, CornerVectors[3].x),
+            MaxX = Mathf.Max(CornerVectors[0].x, CornerVectors[1].x, CornerVectors[2].x, CornerVectors[3].x),
+            MinY = Mathf.Min(CornerVectors[0].y, CornerVectors[1].y, CornerVectors[2].y, CornerVectors[3].y),
+            MaxY = Mathf.Max(CornerVectors[0].y, CornerVectors[1].y, CornerVectors[2].y, CornerVectors[3].y),
+        };
+    }
+
 }
+
+internal record struct Corners(float MinX, float MinY, float MaxX, float MaxY);
