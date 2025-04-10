d43e81654ae733df5e799e20e95f221a23806748
Captain Coder <captaincodersacademy@gmail.com>
2025-04-10 11:49:19 -0700

    chore: fiddling with DieHUD

--- Unity Project/Dungeoneering/Assets/_Libs/CaptainCoder.Dungeoneering.Encounter/src/AttackRollData/DiceHUD.cs
+++ Unity Project/Dungeoneering/Assets/_Libs/CaptainCoder.Dungeoneering.Encounter/src/AttackRollData/DiceHUD.cs
@@ -9,6 +9,7 @@ using NaughtyAttributes;
 using TMPro;
 
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 using UnityEngine.UI;
 
 namespace CaptainCoder.Dungeoneering.Encounter
@@ -24,9 +25,13 @@ namespace CaptainCoder.Dungeoneering.Encounter
         [AssertIsSet][SerializeField] private TextMeshProUGUI _splitLabel;
 
         private int _damage;
+        private int _damageBonus;
         private int _accuracy;
+        private int _accuracyBonus;
         private int _power;
-        private int _split;
+        private int _powerSpent;
+        private int _bonusTotal;
+        private int _requiredAccuracy;
 
 
         void Awake()
@@ -36,27 +41,35 @@ namespace CaptainCoder.Dungeoneering.Encounter
 
         private void HandleDiceResults(IEnumerable<DieResult> result)
         {
+            _damage = 0;
+            _damageBonus = 0;
+            _accuracy = 0;
+            _accuracyBonus = 0;
+            _power = 0;
+            _powerSpent = 0;
+            _bonusTotal = 0;
             RenderDice(result);
-            _damageLabel.text = _damage.ToString();
-            _accuracyLabel.text = _accuracy.ToString();
-            _powerLabel.text = _power.ToString();
-            _splitLabel.text = _split.ToString();
+            UpdateLabels();
             Rebuild();
+        }
+
+        private void UpdateLabels()
+        {
+            _damageLabel.text = $"{_damage + _damageBonus}";
+            _accuracyLabel.text = $"{_accuracy + _accuracyBonus}";
+            _powerLabel.text = $"{_power - _powerSpent}/{_power}";
+            _splitLabel.text = $"{_bonusTotal - _damageBonus - _accuracyBonus}/{_bonusTotal}";
         }
 
         private void RenderDice(IEnumerable<DieResult> result)
         {
-            _damage = 0;
-            _accuracy = 0;
-            _power = 0;
-            _split = 0;
             int ix = 0;
             foreach (DieResult die in result)
             {
                 _damage += die.Damage;
                 _accuracy += die.Accuracy;
                 _power += die.Power;
-                _split += die.Split;
+                _bonusTotal += die.Split;
                 DieFaceRenderer renderer = _dieFaceRenderers[ix++];
                 renderer.RenderDieFace(die.Die, die.Face);
                 renderer.Show();
@@ -84,6 +97,11 @@ namespace CaptainCoder.Dungeoneering.Encounter
             }
         }
 
+        public void SetRequiredAccuracy(int requiredAccuracy)
+        {
+            _requiredAccuracy = requiredAccuracy;
+        }
+
         public void SetDice(IEnumerable<DieData> dice)
         {
             _diceBoxController.SetDice(dice);
@@ -99,9 +117,9 @@ namespace CaptainCoder.Dungeoneering.Encounter
         internal void Roll()
         {
             _damageLabel.text = "?";
-            _accuracyLabel.text = "?";
-            _powerLabel.text = "?";
-            _splitLabel.text = "?";
+            _accuracyLabel.text = $"?";
+            _powerLabel.text = "?/?";
+            _splitLabel.text = "?/?";
             _diceBoxController.Roll();
         }
     }
--- Unity Project/Dungeoneering/Assets/_Libs/CaptainCoder.Dungeoneering.Encounter/src/AttackRollData/DieResult.cs
+++ Unity Project/Dungeoneering/Assets/_Libs/CaptainCoder.Dungeoneering.Encounter/src/AttackRollData/DieResult.cs
@@ -1,8 +1,8 @@
 namespace CaptainCoder.Dungeoneering.Encounter;
 public record struct DieResult(DieData Die, int Face)
 {
-    public readonly int Damage => Die.Faces[Face].Damage;
-    public readonly int Accuracy => Die.Faces[Face].Aim;
+    public readonly int Damage => Die.Faces[Face].IsSplit ? 0 : Die.Faces[Face].Damage;
+    public readonly int Accuracy => Die.Faces[Face].IsSplit ? 0 : Die.Faces[Face].Aim;
     public readonly int Power => Die.Faces[Face].Power;
     public readonly int Split => Die.Faces[Face].IsSplit ? 1 : 0;
 }
